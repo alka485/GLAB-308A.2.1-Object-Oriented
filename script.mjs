a33b7a83b55753a2bbbd5ff2cc48f26ea9c22482
Alka Sah <alkasah485@gmail.com>
2024-05-28 12:55:07 -0700

    Part 2  done

--- script.mjs
+++ script.mjs
@@ -18,4 +18,40 @@ const adventurer = {
         console.log(`${this.name} rolled a ${result}.`)
     }
     }
-    
+ //---------------------------------//
+ //Part 2
+ 
+ class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+      this.roll = 10;
+    }
+  }
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+//--------------------------------//
+//Part 3: Class Features
+
+class Adventurer extends Character {
+    constructor (name, role) {
+      super(name);
+      // Adventurers have specialized roles.
+      this.role = role;
+      // Every adventurer starts with a bed and 50 gold coins.
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout () {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+  }
