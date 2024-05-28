3fd0a6eaeeac7db692d307f20b739ef458bf1a00
Alka Sah <alkasah485@gmail.com>
2024-05-28 12:47:37 -0700

    Part 1 done

---
+++ script.mjs
@@ -0,0 +1,21 @@
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: [
+        {
+        name: "Leo",
+        type: "Cat"
+    },
+        {
+            name: "Frank",
+            type: "Flea"    
+
+    }
+    ],
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+    }
+    }
+    
